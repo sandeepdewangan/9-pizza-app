aad929d7c6efb33ca4e02535f3165dd6125c54aa
Sandeep <sandeepdewangan7@gmail.com>
2025-02-11 12:32:22 +0530

    Calling action and form submission

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import User from "./features/user/User";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -32,6 +34,7 @@ function App() {
         {
           path: "/order/new",
           element: <CreateOrder />,
+          action: createOrderAction,
         },
         {
           path: "/order:orderId",
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,9 +1,25 @@
+import { Form, redirect } from "react-router-dom";
+
 const CreateOrder = () => {
   return (
     <div>
       <h1>CreateOrder</h1>
+      <Form method="POST">
+        <input type="text" name="fullname" />
+        <input type="text" name="mobile" />
+        <button>Add</button>
+      </Form>
     </div>
   );
 };
+
+// whenever form is submitted this function calls.
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  // call actions from here
+  return redirect("/menu");
+}
 
 export default CreateOrder;
